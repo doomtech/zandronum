4bd425c56e1a1899695cf8af8bb7483f151dbf69
Phillip Cohen <philltopia@gmail.com>
2007-08-13 04:14:34 +0000

    - Rank/spread/DM stats and team scores now show up in the ZDoom HUD.
    SVN r587 (trunk)

--- src/scoreboard.cpp
+++ src/scoreboard.cpp
@@ -186,7 +186,7 @@ static	void			scoreboard_DrawBottomString( void );
 
 // [RC] A flag to hide the 'spectating...' string for screenshots or movies.
 CVAR (Bool, r_drawspectatingstring, true, CVAR_ARCHIVE|CVAR_GLOBALCONFIG);
-
+EXTERN_CVAR( Bool, cl_stfullscreenhud);
 //*****************************************************************************
 //	FUNCTIONS
 
@@ -703,7 +703,7 @@ void SCOREBOARD_RenderDMStats( void )
 	}
 
 	// In fullscreen, don't display anything else. We have another display for that.
-	if (( realviewheight == SCREENHEIGHT ) && viewactive )
+	if (( realviewheight == SCREENHEIGHT ) && viewactive && cl_stfullscreenhud )
 		return;
 
 	ulYPos = ST_Y - ( ulTextHeight * 2 ) + 1;
@@ -1061,7 +1061,7 @@ void SCOREBOARD_RenderTeamStats( player_s *pPlayer )
 
 	// Don't display scores or anything if the status bar is minimized. We
 	// have a different display for that.
-	if (( realviewheight == SCREENHEIGHT ) && viewactive )
+	if (( realviewheight == SCREENHEIGHT ) && viewactive && cl_stfullscreenhud)
 		return;
 
 	// Now, draw the blue and red team scores.
