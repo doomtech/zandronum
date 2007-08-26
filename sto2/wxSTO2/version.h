6a3e272af919c0a2a1ca0f093d7d1dbbfc465dbe
Phillip Cohen <philltopia@gmail.com>
2007-08-26 20:27:25 +0000

    - Added the first version of STO2, showing a simple GUI.
    SVN r650 (trunk)

---
+++ sto2/wxSTO2/version.h
@@ -0,0 +1,16 @@
+//-----------------------------------------------------------------------------
+//
+// Skulltag Online 2
+// Copyright (C) 2007 Rivecoder
+// Date created: 8/26/07
+//
+// Version numbers.
+//-----------------------------------------------------------------------------
+
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+// Retrieve our latest revision info.
+#include "svnrevision.h"
+
+#endif
