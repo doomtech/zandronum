8a5d358bf6b8fb6ea9dcc71f6b6a0b3672bcf061
Benjamin Berkels <torr.samaho@quantentunnel.de>
2008-06-15 19:55:42 +0000

    bumped version to 97d2-RC3
    SVN r1439 (trunk)

--- src/version.h
+++ src/version.h
@@ -40,7 +40,7 @@
 
 /** Lots of different version numbers **/
 
-#define DOTVERSIONSTR "0.97d2-RC2"
+#define DOTVERSIONSTR "0.97d2-RC3"
 #define DOTVERSIONSTR_NOREV DOTVERSIONSTR
 
 // [BB] The version string that includes revision / compatibility data.
