425d81aa6bb36314514ebe93c31ed2a714c0714c
Benjamin Berkels <torr.samaho@quantentunnel.de>
2008-06-23 18:04:31 +0000

    Changed: Clients are now always informed when other players are pressing any attack buttons. This is done in a way that doesn't increase net traffic at all and fixes several problems: - Now fully fixed: Attack animations are not always displayed properly in demos if you are spying a player that you didn't spy when recording the demo. - When using coop spy, other players may get stuck in the attack state locally.
    SVN r1470 (trunk)

--- src/cl_main.cpp
+++ src/cl_main.cpp
@@ -3622,7 +3622,8 @@ static void client_MovePlayer( BYTESTREAM_s *pByteStream )
 	ulPlayer = NETWORK_ReadByte( pByteStream );
 
 	// Is this player visible? If not, there's no other information to read in.
-	bVisible = !!NETWORK_ReadByte( pByteStream );
+	ULONG ulFlags = NETWORK_ReadByte( pByteStream );
+	bVisible = ( ulFlags & PLAYER_VISIBLE );
 
 	// The server only sends position, angle, etc. information if the player is actually
 	// visible to us.
@@ -3687,6 +3688,18 @@ static void client_MovePlayer( BYTESTREAM_s *pByteStream )
 	{
 		P_CrouchMove( &players[ulPlayer], -1 );
 	}
+
+	// [BB] Set whether the player is attacking or not.
+	// Check: Is it a good idea to only do this, when the player is visible?
+	if ( ulFlags & PLAYER_ATTACK )
+		players[ulPlayer].cmd.ucmd.buttons |= BT_ATTACK;
+	else
+		players[ulPlayer].cmd.ucmd.buttons &= ~BT_ATTACK;
+
+	if ( ulFlags & PLAYER_ALTATTACK )
+		players[ulPlayer].cmd.ucmd.buttons |= BT_ALTATTACK;
+	else
+		players[ulPlayer].cmd.ucmd.buttons &= ~BT_ALTATTACK;
 }
 
 //*****************************************************************************
@@ -4005,6 +4018,9 @@ static void client_SetPlayerState( BYTESTREAM_s *pByteStream )
 		// [BB] It's necessary at all, because the server only informs a client about the cmd.ucmd.buttons
 		// value (containing the information if a player uses BT_ATTACK or BT_ALTATTACK) of the player
 		// who's eyes the client is spying through.
+		// [BB] SERVERCOMMANDS_MovePlayer/client_MovePlayer now informs a client about BT_ATTACK or BT_ALTATTACK
+		// of every player. This hopefully properly fixes these problems once and for all.
+		/*
 		if ( ( CLIENTDEMO_IsPlaying( ) || ( ( GAMEMODE_GetFlags( GAMEMODE_GetCurrentMode( )) & GMF_COOPERATIVE ) && ulPlayer != consoleplayer ) )
 				&& players[ulPlayer].ReadyWeapon )
 		{
@@ -4014,6 +4030,7 @@ static void client_SetPlayerState( BYTESTREAM_s *pByteStream )
 				P_SetPsprite (&players[ulPlayer], ps_weapon, players[ulPlayer].ReadyWeapon->GetAltAtkState(!!players[ulPlayer].refire));
 
 		}
+		*/
 		break;
 	case STATE_PLAYER_ATTACK2:
 
@@ -4642,6 +4659,8 @@ static void client_UpdatePlayerExtraData( BYTESTREAM_s *pByteStream )
 	}
 	players[ulPlayer].mo->pitch = lPitch;
 	players[ulPlayer].mo->waterlevel = ulWaterLevel;
+	// [BB] The attack buttons are now already set in *_MovePlayer, so additionally setting
+	// them here is obsolete. I don't want to change this before 97D2 final though.
 	players[ulPlayer].cmd.ucmd.buttons = ulButtons;
 //	players[ulPlayer].momx = lMomX;
 //	players[ulPlayer].momy = lMomY;
