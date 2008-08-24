31c63d9892b316cd9072a638a8358c3c77bdc42c
Benjamin Berkels <torr.samaho@quantentunnel.de>
2008-08-24 06:46:22 +0000

    Updated the branch with the latest changes from the main trunk (revision 1578-1598): - Fixed: In game modes that reset, the server tells newly connecting clients to spawn certain kind of actors even though they are already destroyed, e.g. exploded ExplosiveBarrels. - Fixed: When a player respawns while a time freezer owned by another player is active, the respawned player can't move anymore as long as the freezer is active. - Fixed: In cooperative game modes, when the players travel from any map to a map with fewer player starts than players, some of them may end up in a zombie like state. - Fixed: When an Archvile raises a monster online, on the clients the total monsters counter is increased by two instead of one.
    SVN r1603 (latestzdoom)

--- src/g_level.cpp
+++ src/g_level.cpp
@@ -3035,6 +3035,13 @@ void G_FinishTravel ()
 		{
 			oldpawn = pawndup;
 
+			// [BB] On a map with fewer player starts than players, some player already
+			// will be telefragged here. To ensure that they can respawn properly
+			// and don't end up in a zombie state, let them be reborn when respawn
+			// the next time.
+			if ( pawn->player->health <= 0 )
+				pawn->player->playerstate = PST_REBORN;
+
 			// The player being spawned here is a short lived dummy and
 			// must not start any ENTER script or big problems will happen.
 			//pawndup = P_SpawnPlayer (&playerstarts[pawn->player - players], true);
