b27b19242a735ba4f429b196ed191ec2ed130f83
Benjamin Berkels <torr.samaho@quantentunnel.de>
2009-01-18 19:21:03 +0000

    Converted Megasphere and Berserk to DECORATE (ported from ZDoom).
    SVN r1949 (latestzdoom)

--- src/g_doom/a_doomartifacts.cpp
+++ src/g_doom/a_doomartifacts.cpp
@@ -15,104 +15,6 @@
 #include "a_doomglobal.h"
 #include "announcer.h"
 
-// Mega sphere --------------------------------------------------------------
-
-class AMegasphere : public AInventory
-{
-	DECLARE_ACTOR (AMegasphere, AInventory)
-public:
-	virtual bool Use (bool pickup)
-	{
-		player_t *player = Owner->player;
-
-		ABasicArmorPickup *armor = static_cast<ABasicArmorPickup *> (Spawn ("BlueArmor", 0,0,0, NO_REPLACE));
-		if (!armor->TryPickup (Owner))
-		{
-			armor->Destroy ();
-		}
-
-		// [BB] Break out to prevent a crash.
-		if ( player == NULL )
-			return true;
-
-		// [BC] Factor in the player's max. health bonus.
-		player->health += deh.MegasphereHealth;
-		if ( player->cheats & CF_PROSPERITY )
-		{
-			if ( player->health > deh.MaxSoulsphere + 50 )
-				player->health = deh.MaxSoulsphere + 50;
-		}
-		else
-		{
-			if ( player->health > deh.MegasphereHealth + player->lMaxHealthBonus )
-				player->health = deh.MegasphereHealth + player->lMaxHealthBonus;
-		}
-
-		player->mo->health = player->health;
-		return true;
-	}
-};
-
-FState AMegasphere::States[] =
-{
-	S_BRIGHT (MEGA, 'A',	6, NULL 				, &States[1]),
-	S_BRIGHT (MEGA, 'B',	6, NULL 				, &States[2]),
-	S_BRIGHT (MEGA, 'C',	6, NULL 				, &States[3]),
-	S_BRIGHT (MEGA, 'D',	6, NULL 				, &States[0])
-};
-
-IMPLEMENT_ACTOR (AMegasphere, Doom, 83, 132)
-	// [BC] Added MF_NOGRAVITY.
-	PROP_Flags (MF_SPECIAL|MF_COUNTITEM|MF_NOGRAVITY)
-	PROP_Inventory_FlagsSet (IF_AUTOACTIVATE|IF_ALWAYSPICKUP)
-	PROP_Inventory_MaxAmount (0)
-	PROP_SpawnState (0)
-	PROP_Inventory_PickupMessage("$GOTMSPHERE")
-	// [BC]
-	PROP_Inventory_PickupAnnouncerEntry( "megasphere" )
-END_DEFAULTS
-
-// Berserk ------------------------------------------------------------------
-
-class ABerserk : public APowerupGiver
-{
-	DECLARE_ACTOR (ABerserk, APowerupGiver)
-public:
-	virtual bool Use (bool pickup)
-	{
-		P_GiveBody (Owner, 100);
-		if (Super::Use (pickup))
-		{
-			const PClass *fistType = PClass::FindClass ("Fist");
-			if (Owner->player->ReadyWeapon == NULL ||
-				Owner->player->ReadyWeapon->GetClass() != fistType)
-			{
-				AInventory *fist = Owner->FindInventory (fistType);
-				if (fist != NULL)
-				{
-					Owner->player->PendingWeapon = static_cast<AWeapon *> (fist);
-				}
-			}
-			return true;
-		}
-		return false;
-	}
-};
-
-FState ABerserk::States[] =
-{
-	S_BRIGHT (PSTR, 'A',   -1, NULL 				, NULL)
-};
-
-IMPLEMENT_ACTOR (ABerserk, Doom, 2023, 134)
-	PROP_Flags (MF_SPECIAL|MF_COUNTITEM)
-	PROP_SpawnState (0)
-	PROP_Inventory_FlagsSet (IF_AUTOACTIVATE|IF_ALWAYSPICKUP)
-	PROP_Inventory_MaxAmount (0)
-	PROP_PowerupGiver_Powerup ("PowerStrength")
-	PROP_Inventory_PickupMessage("$GOTBERSERK")
-END_DEFAULTS
-
 // [BC] Random Powerup ------------------------------------------------------
 
 #define	RPF_MEGASPHERE				1
@@ -212,7 +114,7 @@ bool ARandomPowerup::Use (bool pickup)
 	// Megasphere.
 	case 0:
 
-		pType = RUNTIME_CLASS( AMegasphere );
+		pType = PClass::FindClass( "Megasphere" );
 		break;
 	// Soulsphere.
 	case 1:
