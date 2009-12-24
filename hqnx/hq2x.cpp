baa9fba16ebab6608f0619b2313e2bff4dabc467
Benjamin Berkels <torr.samaho@quantentunnel.de>
2009-12-24 13:49:34 +0000

    Minor cleanup.
    Note: (Already several months ago) I got Maxim Stepin's permission to use the hqnx code under the LGPL.
    
    SVN r2607 (latestzdoom)

--- hqnx/hq2x.cpp
+++ hqnx/hq2x.cpp
@@ -1,7 +1,7 @@
 //hq2x filter demo program
 //----------------------------------------------------------
 //Copyright (C) 2003 MaxSt ( maxst@hiend3d.com )
-
+//
 //This program is free software; you can redistribute it and/or
 //modify it under the terms of the GNU Lesser General Public
 //License as published by the Free Software Foundation; either
