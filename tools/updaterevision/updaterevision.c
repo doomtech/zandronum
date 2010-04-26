89f016165baa75878ec61d4b4100637e522fa052
Benjamin Berkels <torr.samaho@quantentunnel.de>
2010-04-26 20:33:12 +0000

    out of sequence fix backport from ZDoom revision 928+2161: - Fixed updaterevision for Vista: tmpnam() creates a path to the root directory of the drive, which is normally protected on Vista. The tempnam() function uses the TMP variable for a path instead. - tempnam would set the errno to 2 on the Mac even though everything seemed to be working as expected.
    SVN r2751 (latestzdoom)

--- tools/updaterevision/updaterevision.c
+++ tools/updaterevision/updaterevision.c
@@ -30,26 +30,37 @@ int main(int argc, char **argv)
 	// Use svnversion to get the revision number. If that fails, pretend it's
 	// revision 0. Note that this requires you have the command-line svn tools installed.
 	sprintf (run, "svnversion -cn %s", argv[1]);
-	if ((name = tmpnam(NULL)) != NULL &&
-		(stream = freopen(name, "w+b", stdout)) != NULL &&
-		system(run) == 0 &&
-#ifndef __FreeBSD__
-		errno == 0 &&
-#endif
-		fseek(stream, 0, SEEK_SET) == 0 &&
-		fgets(currev, sizeof currev, stream) == currev &&
-		(isdigit(currev[0]) || (currev[0] == '-' && currev[1] == '1')))
+	if ((name = tempnam(NULL, "svnout")) != NULL)
 	{
-		gotrev = 1;
+#ifdef __APPLE__
+		// tempnam will return errno of 2 even though it is successful for our purposes.
+		errno = 0;
+#endif
+		if((stream = freopen(name, "w+b", stdout)) != NULL &&
+		   system(run) == 0 &&
+#ifndef __FreeBSD__
+		   errno == 0 &&
+#endif
+		   fseek(stream, 0, SEEK_SET) == 0 &&
+		   fgets(currev, sizeof currev, stream) == currev &&
+		   (isdigit(currev[0]) || (currev[0] == '-' && currev[1] == '1')))
+		{
+			gotrev = 1;
+		}
 	}
 	if (stream != NULL)
 	{
 		fclose (stream);
 		remove (name);
 	}
+	if (name != NULL)
+	{
+		free (name);
+	}
 
 	if (!gotrev)
 	{
+		fprintf (stderr, "Failed to get current revision: %s\n", strerror(errno));
 		strcpy (currev, "0");
 		rev = currev;
 	}
