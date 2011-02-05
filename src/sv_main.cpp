70adbac9e43a2944c209c38efd40152f55a6c805
Benjamin Berkels <torr.samaho@quantentunnel.de>
2011-02-05 23:28:39 +0000

    Fixed: Under some (other) circumstances a client wrongly displayed an "in console" icon over a player's head.
    SVN r3085 (latestzdoom)

--- src/sv_main.cpp
+++ src/sv_main.cpp
@@ -2284,11 +2284,9 @@ void SERVER_SendFullUpdate( ULONG ulClient )
 		// [BB] Send the number of lives left.
 		SERVERCOMMANDS_SetPlayerLivesLeft( ulIdx, ulClient, SVCF_ONLYTHISCLIENT );
 
-		// [BB] If necessary, also tell this player's chat / console status to the new client.
-		if ( players[ulIdx].bChatting )
-			SERVERCOMMANDS_SetPlayerChatStatus( ulIdx, ulClient, SVCF_ONLYTHISCLIENT );
-		if ( players[ulIdx].bInConsole )
-			SERVERCOMMANDS_SetPlayerConsoleStatus( ulIdx, ulClient, SVCF_ONLYTHISCLIENT );
+		// [BB] Also tell this player's chat / console status to the new client.
+		SERVERCOMMANDS_SetPlayerChatStatus( ulIdx, ulClient, SVCF_ONLYTHISCLIENT );
+		SERVERCOMMANDS_SetPlayerConsoleStatus( ulIdx, ulClient, SVCF_ONLYTHISCLIENT );
 	}
 
 	// Server may have already picked a team for the incoming player. If so, tell him!
