29400ca2cc432c1031f3800de92444b71dc2fcbd
Benjamin Berkels <torr.samaho@quantentunnel.de>
2011-04-11 11:49:15 +0000

    out of sequence fix backport from ZDoom revision 2188: - Fixed: snd_musicvolume needs to check GSnd for NULL, since somebody might have set an atexit for it, which gets executed after the sound system shuts down. - Fixed: FPlayList::Backup() failed to wrap around below entry 0 because Position is unsigned now.
    Note: I already made the first one of the fixes independently in Skulltag a while ago. Now just the formatting is changed to be identical to the ZDoom version.
    
    SVN r3152 (latestzdoom)

--- src/sound/i_music.cpp
+++ src/sound/i_music.cpp
@@ -105,9 +105,10 @@ CUSTOM_CVAR (Float, snd_musicvolume, 0.5f, CVAR_ARCHIVE|CVAR_GLOBALCONFIG)
 	else
 	{
 		// Set general music volume.
-		// [BB] GSnd is probably already NULL when snd_musicvolume is saved to the ini on exit.
-		if ( GSnd != NULL )
+		if (GSnd != NULL)
+		{
 			GSnd->SetMusicVolume(clamp<float>(self * relative_volume, 0, 1));
+		}
 		// For music not implemented through the digital sound system,
 		// let them know about the change.
 		if (currSong != NULL)
