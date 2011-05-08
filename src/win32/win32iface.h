1c9446c80d8afbf87c20a6902a2a257597d62999
Benjamin Berkels <torr.samaho@quantentunnel.de>
2011-05-08 17:02:48 +0000

    out of sequence fix backport from ZDoom revision 2661: - Disable framebuffer debug spew. All those OutputDebugString messages from DCanvas::DrawLine()'s Lock and Unlock calls were slowing things down extremely when looking at a software-drawn automap.
    SVN r3222 (latestzdoom)

--- src/win32/win32iface.h
+++ src/win32/win32iface.h
@@ -424,7 +424,7 @@ private:
 #define LOG4(x,y,z,a,b)	do { if (dbg) { fprintf (dbg, x, y, z, a, b); fflush (dbg); } } while(0)
 #define LOG5(x,y,z,a,b,c) do { if (dbg) { fprintf (dbg, x, y, z, a, b, c); fflush (dbg); } } while(0)
 FILE *dbg;
-#elif _DEBUG
+#elif _DEBUG && 0
 #define STARTLOG
 #define STOPLOG
 #define LOG(x)			{ OutputDebugString(x); }
