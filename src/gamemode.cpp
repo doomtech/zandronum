789de84e8d904f5289e0e536ccea82906f926a65
Benjamin Berkels <torr.samaho@quantentunnel.de>
2011-05-14 23:38:31 +0000

    In gamemodes that reset the map time during a map reset, the timelimit counter is now hidden on the score board while the game is not in progress.
    SVN r3233 (latestzdoom)

--- src/gamemode.cpp
+++ src/gamemode.cpp
@@ -386,6 +386,10 @@ bool GAMEMODE_IsGameInProgressOrResultSequence( void )
 //
 bool GAMEMODE_IsTimelimitActive( void )
 {
+	// [BB] In gamemodes that reset the time during a map reset, the timelimit doesn't make sense when the game is not in progress.
+	if ( ( GAMEMODE_GetFlags( GAMEMODE_GetCurrentMode( )) & GMF_MAPRESET_RESETS_MAPTIME ) && ( GAMEMODE_IsGameInProgress( ) == false ) )
+		return false;
+
 	// [BB] Teamlms doesn't support timelimit, so just turn it off in this mode.
 	if ( teamlms )
 		return false;
