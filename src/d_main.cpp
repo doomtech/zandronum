c26027e988a013993e9c806ff326ce9d5973eb35
Benjamin Berkels <torr.samaho@quantentunnel.de>
2012-06-09 16:26:23 +0200

    Since skulltag_data.pk3 is not auto loaded anymore, shareware IWADs are supported again.

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -2015,9 +2015,6 @@ void D_DoomMain (void)
 			D_AddConfigWads(file);
 		}
 	}
-	else
-		I_FatalError ("Due to restrictions of the shareware license "GAMENAME" may not be run with a shareware IWAD.\n");
-
 
 	// Run automatically executed files
 	execFiles = new DArgs;
