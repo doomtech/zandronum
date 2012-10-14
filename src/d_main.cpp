f5c717dd45e344be40ef5b2c22bea23e24c778f7
Benjamin Berkels <torr.samaho@quantentunnel.de>
2012-10-14 10:23:21 +0200

    renamed compat_oldbridgedrops to compat_bridgedrops

--- src/d_main.cpp
+++ src/d_main.cpp
@@ -630,7 +630,7 @@ CVAR (Flag, compat_oldrandom,		compatflags2, COMPATF2_OLD_RANDOM_GENERATOR);
 CVAR (Flag, compat_nogravity_spheres,		compatflags2, COMPATF2_NOGRAVITY_SPHERES);
 CVAR (Flag, compat_dont_stop_player_scripts_on_disconnect,		compatflags2, COMPATF2_DONT_STOP_PLAYER_SCRIPTS_ON_DISCONNECT);
 CVAR (Flag, compat_oldexplosionthrust,		compatflags2, COMPATF2_OLD_EXPLOSION_THRUST);
-CVAR (Flag, compat_oldbridgedrops,		compatflags2, COMPATF2_OLD_BRIDGE_DROPS);
+CVAR (Flag, compat_bridgedrops,		compatflags2, COMPATF2_OLD_BRIDGE_DROPS);
 
 //==========================================================================
 //
