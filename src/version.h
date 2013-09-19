8324e10ff9f72d5d98529e71c562292b6aa69e9f
Benjamin Berkels <torr.samaho@quantentunnel.de>
2013-09-19 19:27:04 +0200

    Ported changes from ZDoom revision 1854-1855 and from GZDoom revision 463: - Gez's misc. bugs patch:   * Moves the dog sound out of the Doom-specific sounds in SNDINFO to address this,   * Renames the dog actor to MBFHelperDog to prevent name conflicts,   * Adds APROP_Score to CheckActorProperty, - Gez's latest cleanup patch. - Fixed: Unmorphing while invulnerable was blocked.

--- src/version.h
+++ src/version.h
@@ -47,8 +47,8 @@
 #define DOTVERSIONSTR_NOREV DOTVERSIONSTR
 
 #define ZDVER_STRING "2.3.1"
-#define ZD_SVN_REVISION_STRING "1853"
-#define ZD_SVN_REVISION_NUMBER 1853
+#define ZD_SVN_REVISION_STRING "1855"
+#define ZD_SVN_REVISION_NUMBER 1855
 
 // [BB] The version string that includes revision / compatibility data.
 #define DOTVERSIONSTR_REV DOTVERSIONSTR "-r" SVN_REVISION_STRING
