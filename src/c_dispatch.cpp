fb6c4fb94c3b3e5ad4e44e89845519959adb88a6
Benjamin Berkels <torr.samaho@quantentunnel.de>
2014-01-04 11:27:26 +0100

    Fixed: The view was not centered in demos when the recording player released the mouse look button and lookspring was true.

--- src/c_dispatch.cpp
+++ src/c_dispatch.cpp
@@ -56,6 +56,7 @@
 #include "p_local.h"
 #include "g_level.h"
 #include "p_acs.h"
+#include "cl_demo.h"
 
 // MACROS ------------------------------------------------------------------
 
@@ -594,6 +595,10 @@ void C_DoCommand (const char *cmd, int keynum)
 					{
 						if ( players[consoleplayer].mo )
 							players[consoleplayer].mo->pitch = 0;
+
+						// [BB] We need to record this for the demo.
+						if ( CLIENTDEMO_IsRecording( ))
+							CLIENTDEMO_WriteLocalCommand( CLD_CENTERVIEW, NULL );
 					}
 				}
 			}
