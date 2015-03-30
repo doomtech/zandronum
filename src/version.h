ff6ac8843c33325886c4c96f02697ba1b047e91e
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-03-30 19:17:03 +0200

    - changed the version string to 2.1-alpha - changed BUILD_ID/BUILD_ID_STR to internal

--- src/version.h
+++ src/version.h
@@ -42,8 +42,8 @@
 
 #define GAME_MAJOR_VERSION 1
 #define GAME_MINOR_VERSION 2
-#define GAMEVER_STRING "2.0"
-#define DOTVERSIONSTR GAMEVER_STRING
+#define GAMEVER_STRING "2.1"
+#define DOTVERSIONSTR GAMEVER_STRING "-alpha"
 #define DOTVERSIONSTR_NOREV DOTVERSIONSTR
 
 #define ZDVER_STRING "2.5.0"
@@ -66,8 +66,8 @@
 #define BUILD_PRIVATE		3
 
 // [RC] Release code ID for this build.
-#define BUILD_ID			BUILD_RELEASE
-#define BUILD_ID_STR		"Release" // Used in the exe's metadata.
+#define BUILD_ID			BUILD_INTERNAL
+#define BUILD_ID_STR		"Internal" // Used in the exe's metadata.
 
 // Version identifier for network games.
 // Bump it every time you do a release unless you're certain you
