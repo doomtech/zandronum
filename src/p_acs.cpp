1d0a0f8325812a9bfce24b425915bfaf41c14eff
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-01 14:01:07 +0200

    ported changes from ZDoom revision 3402 and from GZDoom revision 1298-1301
    * Updated to ZDoom r3402:
    - Fixed: When processing the SFLG and SVCT chunks in ACS objects, the script number must be read as a signed integer.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1298 b0f79afe-0144-0410-b225-9a4edf0717df
    
    - Everyone loves crashes, right?
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1299 b0f79afe-0144-0410-b225-9a4edf0717df
    
    - Use 1-unit increments for ambient light slider.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1300 b0f79afe-0144-0410-b225-9a4edf0717df
    
    - reverted step size change of ambient light slider.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1301 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/p_acs.cpp
+++ src/p_acs.cpp
@@ -2358,6 +2358,7 @@ void FBehavior::LoadScriptsDirectory ()
 		BYTE *b;
 		DWORD *dw;
 		WORD *w;
+		SWORD *sw;
 		ScriptPtr2 *po;		// Old
 		ScriptPtr1 *pi;		// Intermediate
 		ScriptPtr3 *pe;		// LittleEnhanced
@@ -2480,7 +2481,7 @@ void FBehavior::LoadScriptsDirectory ()
 		scripts.dw += 2;
 		for (i = max; i > 0; --i, scripts.w += 2)
 		{
-			ScriptPtr *ptr = const_cast<ScriptPtr *>(FindScript (LittleShort(scripts.w[0])));
+			ScriptPtr *ptr = const_cast<ScriptPtr *>(FindScript (LittleShort(scripts.sw[0])));
 			if (ptr != NULL)
 			{
 				ptr->Flags = LittleShort(scripts.w[1]);
@@ -2496,7 +2497,7 @@ void FBehavior::LoadScriptsDirectory ()
 		scripts.dw += 2;
 		for (i = max; i > 0; --i, scripts.w += 2)
 		{
-			ScriptPtr *ptr = const_cast<ScriptPtr *>(FindScript (LittleShort(scripts.w[0])));
+			ScriptPtr *ptr = const_cast<ScriptPtr *>(FindScript (LittleShort(scripts.sw[0])));
 			if (ptr != NULL)
 			{
 				ptr->VarCount = LittleShort(scripts.w[1]);
