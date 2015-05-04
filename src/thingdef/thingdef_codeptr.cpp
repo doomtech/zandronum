188f3b97bad680dae33e6c24f216d3affc89f5cc
edward_san <edo88@email.it>
2015-05-04 22:57:08 +0200

    - Fixed: A_FadeOut did not work for actors which don't have a network id (addresses 1986).

--- src/thingdef/thingdef_codeptr.cpp
+++ src/thingdef/thingdef_codeptr.cpp
@@ -2766,12 +2766,8 @@ DEFINE_ACTION_FUNCTION_PARAMS(AActor, A_FadeOut)
 	ACTION_PARAM_BOOL(remove, 1);
 
 	// [BB] This is handled server-side.
-	if (( NETWORK_GetState( ) == NETSTATE_CLIENT ) ||
-		( CLIENTDEMO_IsPlaying( )))
-	{
-		if (( self->ulNetworkFlags & NETFL_CLIENTSIDEONLY ) == false )
-			return;
-	}
+	if ( NETWORK_InClientModeAndActorNotClientHandled( self ) )
+		return;
 
 	if (reduce == 0)
 	{
