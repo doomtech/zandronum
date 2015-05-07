e62666066c40ae6607e23e3a071fea1794e0c74a
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-07 21:01:09 +0200

    ported changes from ZDoom revision 3608-3612 and from GZDoom revision 1372-1373
    * Updated to ZDoom r3611:
    - Fixed: When PlayerIsGone() destroys a morphed player, it needs to destroy the unmorphed player too.
    - Fixed: Destroying a morphed player would try to unmorph the player twice.
    - P_SeekerMissile() now returns true when the projectile's speed is 0 and the tracer would otherwise be seekable. Fixes 0-speed missiles using A_SeekerMissile for special effects.
    - Instead of a huge switch statement, lets convert POV to axes by bitwise operations since we later assume we can do that for the button mapping anyways.
    - Fixed: r_translate.cpp:SetRange() did not set the true color palette entry for single-entry ranges.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1372 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom r3612:
    - Fixed: powered up weapons must not be added to the weapon slots.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1373 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/version.h
+++ src/version.h
@@ -51,8 +51,8 @@ unsigned int GetRevisionNumber();
 #define VERSIONSTR DOTVERSIONSTR
 
 #define ZDVER_STRING "2.5.0"
-#define ZD_SVN_REVISION_STRING "3607"
-#define ZD_SVN_REVISION_NUMBER 3607
+#define ZD_SVN_REVISION_STRING "3612"
+#define ZD_SVN_REVISION_NUMBER 3612
 
 // [BB] The version string that includes revision / compatibility data.
 #define DOTVERSIONSTR_REV DOTVERSIONSTR "-r" SVN_REVISION_STRING
