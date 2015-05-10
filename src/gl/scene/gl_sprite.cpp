00041f9f395c52fe28eac04bed22f465b878b89a
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-10 22:33:08 +0200

    ported changes from GZDoom revision 1501
    - Added gl_billboard_particles. If true, particles are billboarded to face the camera, if false the old behavior applies.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1501 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/gl/scene/gl_sprite.cpp
+++ src/gl/scene/gl_sprite.cpp
@@ -70,6 +70,7 @@ CVAR(Float, gl_sclipthreshold, 10.0, CVAR_ARCHIVE)
 CVAR(Float, gl_sclipfactor, 1.8, CVAR_ARCHIVE)
 CVAR(Int, gl_particles_style, 2, CVAR_ARCHIVE | CVAR_GLOBALCONFIG) // 0 = square, 1 = round, 2 = smooth
 CVAR(Int, gl_billboard_mode, 0, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
+CVAR(Bool, gl_billboard_particles, true, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CVAR(Int, gl_enhanced_nv_stealth, 3, CVAR_ARCHIVE | CVAR_GLOBALCONFIG)
 CUSTOM_CVAR(Int, gl_fuzztype, 0, CVAR_ARCHIVE)
 {
@@ -217,7 +218,7 @@ void GLSprite::Draw(int pass)
 	if (!modelframe)
 	{
 		// [BB] Billboard stuff
-		const bool drawWithXYBillboard = ( !(actor && actor->renderflags & RF_FORCEYBILLBOARD)
+		const bool drawWithXYBillboard = ( (particle && gl_billboard_particles) || !(actor && actor->renderflags & RF_FORCEYBILLBOARD)
 		                                   //&& GLRenderer->mViewActor != NULL
 		                                   && (gl_billboard_mode == 1 || (actor && actor->renderflags & RF_FORCEXYBILLBOARD )) );
 		gl_RenderState.Apply();
