9d395a80d0e64af6c9a6ca90751cc53faf3c9642
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-12 08:15:59 +0200

    ported changes from ZDoom revision 4072-4120 and from GZDoom revision 1518-1523
    * Updated to ZDoom 4081:
    - Updated DUMB revision.
    - Added Oktalyzer support.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1518 b0f79afe-0144-0410-b225-9a4edf0717df
    
    - Added DUMBEXPORT macro to Oktalyzer loaders so that they accept to compile.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1519 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4090:
    - Updated DUMB revision.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1520 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4100:
    - Updated DUMB revision.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1521 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4110:
    - Updated DUMB revision.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1522 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4120:
    - Updated DUMB revision.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1523 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/version.h
+++ src/version.h
@@ -51,8 +51,8 @@ unsigned int GetRevisionNumber();
 #define VERSIONSTR DOTVERSIONSTR
 
 #define ZDVER_STRING "2.6.1"
-#define ZD_SVN_REVISION_STRING "4071"
-#define ZD_SVN_REVISION_NUMBER 4071
+#define ZD_SVN_REVISION_STRING "4120"
+#define ZD_SVN_REVISION_NUMBER 4120
 
 // [BB] The version string that includes revision / compatibility data.
 #define DOTVERSIONSTR_REV DOTVERSIONSTR "-r" SVN_REVISION_STRING
