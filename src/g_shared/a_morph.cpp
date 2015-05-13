6aae5a7d65dda5436e42c1a6fe0c9a6dd98f32fe
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-13 20:27:18 +0200

    ported changes from ZDoom revision 4165-4166 and from GZDoom revision 1533
    * Updated to ZDoom 4166:
    - Fixed: GCC build.
    - Added support for Visual Studio to the CMake project. [Untested]
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1533 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/g_shared/a_morph.cpp
+++ src/g_shared/a_morph.cpp
@@ -17,6 +17,9 @@
 
 static FRandom pr_morphmonst ("MorphMonster");
 
+void EndAllPowerupEffects(AInventory *item);
+void InitAllPowerupEffects(AInventory *item);
+
 //---------------------------------------------------------------------------
 //
 // FUNC P_MorphPlayer
