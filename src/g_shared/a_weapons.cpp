5057584da85bab45d4351dc1e5f52cd40305f2c7
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-14 11:58:43 +0200

    ported changes from ZDoom revision 4203-4204 and from GZDoom revision 1547
    * Updated to ZDoom 4204:
    - Added Weapon.MinSelectionAmmo1 and Weapon.MinSelectionAmmo2 to exclude weapons from autoselection even if they have enough ammo to be used.
    - Made FName::NameManager::Inited a static member variable.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1547 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/g_shared/a_weapons.cpp
+++ src/g_shared/a_weapons.cpp
@@ -75,6 +75,10 @@ void AWeapon::Serialize (FArchive &arc)
 		}
 	arc << FOVScale
 		<< Crosshair;
+	if (SaveVersion >= 4203)
+	{
+		arc << MinSelAmmo1 << MinSelAmmo2;
+	}
 }
 
 //===========================================================================
