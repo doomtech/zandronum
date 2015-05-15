b1cb8d4792cbfd9ed547831ab8e4c959a859c2a1
Benjamin Berkels <torr.samaho@quantentunnel.de>
2015-05-15 20:43:42 +0200

    ported changes from ZDoom revision 4297-4301 and from GZDoom revision 1581-1583
    * Updated to ZDoom 4297:
    - Replaced unused RNGs with pr_damagemobj for consistency checksum.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1581 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4299:
    - Fixed the Peasants DECORATE definition.
    - Fixed inappropriate type for FloatBobPhase property.
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1582 b0f79afe-0144-0410-b225-9a4edf0717df
    
    * Updated to ZDoom 4301:
    - Fixed parameter type for Floatbobphase property.
    - Added AFADoomer's patch to set a text highlight color for the list menu.
    
    
    git-svn-id: http://mancubus.net/svn/hosted/gzdoom/trunk@1583 b0f79afe-0144-0410-b225-9a4edf0717df

--- src/p_interaction.cpp
+++ src/p_interaction.cpp
@@ -88,7 +88,7 @@ void SERVERCONSOLE_UpdateScoreboard( void );
 static FRandom pr_obituary ("Obituary");
 static FRandom pr_botrespawn ("BotRespawn");
 static FRandom pr_killmobj ("ActorDie");
-static FRandom pr_damagemobj ("ActorTakeDamage");
+FRandom pr_damagemobj ("ActorTakeDamage");
 static FRandom pr_lightning ("LightningDamage");
 static FRandom pr_poison ("PoisonDamage");
 static FRandom pr_switcher ("SwitchTarget");
--- src/version.h
+++ src/version.h
@@ -51,8 +51,8 @@ unsigned int GetRevisionNumber();
 #define VERSIONSTR DOTVERSIONSTR
 
 #define ZDVER_STRING "2.6.1"
-#define ZD_SVN_REVISION_STRING "4296"
-#define ZD_SVN_REVISION_NUMBER 4296
+#define ZD_SVN_REVISION_STRING "4301"
+#define ZD_SVN_REVISION_NUMBER 4301
 
 // [BB] The version string that includes revision / compatibility data.
 #define DOTVERSIONSTR_REV DOTVERSIONSTR "-r" SVN_REVISION_STRING
